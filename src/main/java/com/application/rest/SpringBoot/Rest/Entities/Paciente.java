c53d4b00a237ed538f8062bf527ee395860d96d2
bspoloo <animetx098@gmail.com>
2024-06-18 20:50:32 -0400

    Para cjp

---
+++ src/main/java/com/application/rest/SpringBoot/Rest/Entities/Paciente.java
@@ -0,0 +1,44 @@
+package com.application.rest.SpringBoot.Rest.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Paciente")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
