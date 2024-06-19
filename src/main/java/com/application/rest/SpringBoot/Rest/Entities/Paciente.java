edc5681c17acf218f2979b6d1806d2cc5ceeb807
bspoloo <animetx098@gmail.com>
2024-06-18 20:52:39 -0400

    Para cjp

--- src/main/java/com/application/rest/SpringBoot/Rest/Entities/Paciente.java
+++ src/main/java/com/application/rest/SpringBoot/Rest/Entities/Paciente.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "Paciente")
 public class Paciente {
     @Id
@@ -17,28 +19,4 @@ public class Paciente {
     private String email;
 
     // Getters y setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
